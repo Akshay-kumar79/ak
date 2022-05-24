a7c793c376d4f965fec817de997beeae2b2aef1d
Akshay kumar shaw <akshay.8459361606@gmail.com>
2022-05-24 13:14:59 +0530

    df

--- app/src/main/java/com/example/ak/firstfragment/FirstFragment.kt
+++ app/src/main/java/com/example/ak/firstfragment/FirstFragment.kt
@@ -19,7 +19,6 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        val fuck = 1
 
         binding = FragmentFirstBinding.inflate(inflater, container, false)
         val sharedPreference = requireContext().getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE)
