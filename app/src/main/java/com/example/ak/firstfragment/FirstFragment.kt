f35fddf13e88a9c8ac8aec51d9771008afcd2b0b
Akshay kumar shaw <akshay.8459361606@gmail.com>
2022-05-24 13:12:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ak/firstfragment/FirstFragment.kt
@@ -0,0 +1,41 @@
+package com.example.ak.firstfragment
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.example.ak.databinding.FragmentFirstBinding
+
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val sharedPreference = requireContext().getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE)
+
+
+        binding.btnSave.setOnClickListener {
+            val name = binding.edit.text.toString()
+            sharedPreference.edit().putString("name", name).apply()
+            Toast.makeText(context, "name saved", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnGet.setOnClickListener {
+            val name = sharedPreference.getString("name", "Akshay")
+            binding.textView.text = name
+        }
+
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/viewmodel/viewmodel.kt
@@ -0,0 +1,20 @@
+package com.example.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class viewmodel : ViewModel() {
+
+    private val _viewModelName = MutableLiveData< String>()
+    val viewModelName: LiveData<String>
+     get() = _viewModelName
+
+    init {
+        _viewModelName.value = "Rakesh singh"
+    }
+
+    fun namechange(){
+        _viewModelName.value = "Rakesh"
+    }
+}
